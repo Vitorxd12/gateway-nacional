6b35bad539121f60729b59701912ccf3441e85bb
jovinull <felipejovinogamerplay@gmail.com>
2026-05-05 17:39:42 -0300

    test: adicionar testes de inicialização do contexto da aplicação

---
+++ src/test/java/br/com/cernebr/gateway_nacional/GatewayNacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cernebr.gateway_nacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayNacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
